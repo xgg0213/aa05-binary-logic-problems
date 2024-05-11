7154734e5645e24a518c4c28e93401a6b0cf1aeb
xgg0213 <m.eringan@gmail.com>
2024-05-11 11:51:26 -0700

    solutions 3-convertToBase16

--- 1-base-conversions/3-convertToBase16.js
+++ 1-base-conversions/3-convertToBase16.js
@@ -4,6 +4,9 @@
 
 const convertToBase16 = element => {
   // Your code here 
+  if (typeof element === 'number') return '0x' + element.toString(16);
+  let decimal = parseInt(element.substring(2), 2);
+  return '0x' + decimal.toString(16);
 };
 
 /******************************************************************************/
